5185c2a9e413c394b3cc13f81cd98d882a2e4ab3
katrinahuangg <77704503+katrinahuangg@users.noreply.github.com>
2021-03-06 14:04:41 -0500

    Update robo_advisor.py
    Added printed outputs and last_refreshed variable

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+line = "------------------------"
+
 # INFO INPUTS
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
@@ -9,12 +11,28 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-print(type(response))
-print(response.status_code)
-print(response.text)
 
-quit()
+#print(type(response)) #> <class 'requests.models.Response'>
+#print(response.status_code) #> 200
+#print(response.text)
 
-print("Hello")
-print("Is this working")
+# INFO OUTPUTS
+
+print(line)
+print("SELECTED SYMBOL: ")
+print(line)
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: time")
+print(line)
+print(f"LATEST DAY: {last_refreshed}")
+print("LATEST CLOSE: $")
+print("RECENT HIGH: $")
+print("RECENT LOW: $")
+print(line)
+print("RECOMMENDATION: BUY!")
+print("BECAUSE: TODO")
+print(line)
+print("HAPPY INVESTING!")
+print(line)
