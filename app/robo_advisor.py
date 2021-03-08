01b8e2d2ecbbe8d70dd33e255f20d9267f781476
katrinahuangg <77704503+katrinahuangg@users.noreply.github.com>
2021-03-08 17:09:10 -0500

    Update robo_advisor.py
    added an if statement to print error message and continue loop if stock data could not be retrieved.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -12,6 +12,7 @@ line = "------------------------"
 selected_tickers = []
 high_prices = []
 low_prices = []
+key = "Meta Data"
 
 # function to convert float or integer to usd-formatted string
 def to_usd(my_price):
@@ -26,7 +27,7 @@ while True:
     # validate other entries
     else:
         if ticker.isalpha() == False or len(str(ticker)) > 5:
-                print("Sorry, couldn't find any trading data for that stock symbol. Make sure that the ticker only contains letters and is a valid length. Please try again.")
+                print("Sorry, the symbol you have entered is not valid. Make sure that the ticker only contains letters and is a valid length. Please try again.")
         else:
             # add selected ticker to list if valid
             selected_tickers.append(ticker)
@@ -45,69 +46,73 @@ for ticker in selected_tickers:
 
     parsed_response = json.loads(response.text)
 
-    # get last refreshed date and time
-    last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
-    # get latest close
-    ts = parsed_response["Time Series (5min)"]
-    dates = list(ts.keys()) #TODO sort to ensure latest day is first
-    latest_day = dates[0]
-    latest_close = ts[latest_day]["4. close"]
-
-    # get recent close (max of all high prices)
-
-    for date in dates:
-        high_price = ts[date]["2. high"]
-        high_prices.append(float(high_price))
-        low_price = ts[date]["3. low"]
-        low_prices.append(float(low_price))
-    
-    recent_high = max(high_prices)
-    recent_low = min(low_prices)
-
-    # csv file writing
-    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"prices.{ticker}.csv")
-    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-
-    with open(csv_file_path, "w") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-        writer.writeheader()
-        for date in dates:
-            prices = ts[date]
-            writer.writerow({
-                "timestamp": date,
-                "open": prices["1. open"],
-                "high": prices["2. high"], 
-                "low": prices["3. low"], 
-                "close": prices["4. close"], 
-                "volume": prices["5. volume"]
-            })
-
-    if float(latest_close) < (float(recent_low) * 1.2): # if latest close is less than 1.2x recent low
-        recommendation = "BUY"
-        recommendation_explanation = "THE STOCK'S LATEST CLOSE IS LESS THAN 20% ABOVE THE RECENT LOW."
+    if key not in parsed_response.keys():
+        print(f"Sorry, couldn't find {ticker} data.")
+        continue
     else:
-        recommendation = "DO NOT BUY"
-        recommendation_explanation = "THE STOCK PRICE IS TOO EXPENSIVE."
+        # get last refreshed date and time
+        last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-    # INFO OUTPUTS
-    print(line)
-    print(f"SELECTED SYMBOL: {ticker}")
-    print(line)
-    print("REQUESTING STOCK MARKET DATA...")
-    print(f"REQUEST AT: {current_datetime}")
-    print(line)
-    print(f"LATEST DAY: {last_refreshed}")
-    print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-    print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-    print(f"RECENT LOW: {to_usd(float(recent_low))}")
-    print(line)
-    print(f"RECOMMENDATION: {recommendation}!")
-    print(f"BECAUSE: {recommendation_explanation}")
-    print(line)
-    print(f"WRITING DATA TO CSV: {csv_file_path}...")
-    print(line)
-    print("HAPPY INVESTING!")
-    print(line)
+        # get latest close
+        ts = parsed_response["Time Series (5min)"]
+        dates = list(ts.keys()) #TODO sort to ensure latest day is first
+        latest_day = dates[0]
+        latest_close = ts[latest_day]["4. close"]
+
+        # get recent close (max of all high prices)
+
+        for date in dates:
+            high_price = ts[date]["2. high"]
+            high_prices.append(float(high_price))
+            low_price = ts[date]["3. low"]
+            low_prices.append(float(low_price))
+        
+        recent_high = max(high_prices)
+        recent_low = min(low_prices)
+
+        # csv file writing
+        csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"prices.{ticker}.csv")
+        csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+        with open(csv_file_path, "w") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+            writer.writeheader()
+            for date in dates:
+                prices = ts[date]
+                writer.writerow({
+                    "timestamp": date,
+                    "open": prices["1. open"],
+                    "high": prices["2. high"], 
+                    "low": prices["3. low"], 
+                    "close": prices["4. close"], 
+                    "volume": prices["5. volume"]
+                })
+
+        if float(latest_close) < (float(recent_low) * 1.2): # if latest close is less than 1.2x recent low
+            recommendation = "BUY"
+            recommendation_explanation = "THE STOCK'S LATEST CLOSE IS LESS THAN 20% ABOVE THE RECENT LOW."
+        else:
+            recommendation = "DO NOT BUY"
+            recommendation_explanation = "THE STOCK PRICE IS TOO EXPENSIVE."
+
+        # INFO OUTPUTS
+        print(line)
+        print(f"SELECTED SYMBOL: {ticker}")
+        print(line)
+        print("REQUESTING STOCK MARKET DATA...")
+        print(f"REQUEST AT: {current_datetime}")
+        print(line)
+        print(f"LATEST DAY: {last_refreshed}")
+        print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+        print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+        print(f"RECENT LOW: {to_usd(float(recent_low))}")
+        print(line)
+        print(f"RECOMMENDATION: {recommendation}!")
+        print(f"BECAUSE: {recommendation_explanation}")
+        print(line)
+        print(f"WRITING DATA TO CSV: {csv_file_path}...")
+        print(line)
+        print("HAPPY INVESTING!")
+        print(line)
 
 
